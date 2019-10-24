19494c5c794a21187adfb0edc7f7bf9b80e4fe0a
Perry Hoekstra <perry.hoekstra@gmail.com>
2019-10-24 15:23:01 -0500

    Initial commit.

---
+++ IOSSearchController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSSearchController
+//
+//  Created by Perry Hoekstra on 10/24/19.
+//  Copyright © 2019 Perry Hoekstra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
